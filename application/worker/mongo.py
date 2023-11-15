0774f951a62e9dc7960b1dcf7c37adf5e880da1e
Helio Correia <helio.as.correia@gmail.com>
2023-11-15 08:41:04 +0000

    Add beanie

--- application/worker/mongo.py
+++ application/worker/mongo.py
@@ -1,13 +1,22 @@
 from sanic import Sanic
-import motor.motor_asyncio
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from beanie import init_beanie
+
 
 app = Sanic.get_app("app")
 
 
 @app.before_server_start
 async def setup_mongo(app, _):
-    app.ctx.mongo_client = motor.motor_asyncio.AsyncIOMotorClient(app.config.MONGO_URL)
-    app.ctx.mongo_db = app.ctx.mongo_client.health
+    app.ctx.mongo_client = AsyncIOMotorClient(app.config.MONGO_URL)
+
+    await init_beanie(
+        database=app.ctx.mongo_client.health,
+        document_models=[
+            "blueprints.measurement.models.Measurement",
+        ],
+    )
 
 
 @app.after_server_stop
